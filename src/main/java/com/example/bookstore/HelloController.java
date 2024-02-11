d228977b1c7803aade6daa5c4c0a467def641622
ammnt <happyzomb@ya.ru>
2024-02-11 12:11:08 +0300

    changed: API endpoint path and response

--- src/main/java/com/example/bookstore/HelloController.java
+++ src/main/java/com/example/bookstore/HelloController.java
@@ -1,14 +1,13 @@
 package com.example.bookstore;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(required = false) String name) {
-        return "Hello, " + name;
+    @GetMapping("/api/v2/**")
+    public String api() {
+        return "It Works!";
     }
 }
